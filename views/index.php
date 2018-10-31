459f486b5259e309525a02e629277f7b1a5d9382
Valerii <valeriisydorov@gmail.com>
2018-10-31 10:06:19 +0200

    Changing the structure of the post on the main page.

--- views/index.php
+++ views/index.php
@@ -1,15 +1,18 @@
 <?php if (is_auth()): ?>
-    <a href="<?= ROOT ?>/add">Add a new article</a>
+    <p>
+        <a href="<?= ROOT ?>/add" class="btn btn-outline-primary">Add a new article</a>
+    </p>
 <?php endif; ?>
 
 <?php foreach ($articles as $article): ?>
-    <time><?= date('j F Y', strtotime($article['date'])) ?></time>
-    <h2><?= $article['title'] ?></h2>
-    <a href="<?= ROOT ?>/view/<?= $article['id'] ?>">Подробнее</a>
-    <?php if (is_auth()): ?>
-        <a href="<?= ROOT ?>/edit/<?= $article['id'] ?>">Изменить</a>
-        <a href="<?= ROOT ?>/delete/<?= $article['id'] ?>"
-           onclick="return confirm('Are you sure you want to delete this article?')">Удалить</a>
-    <?php endif; ?>
+    <div class="mb-4">
+        <time><?= date('j F Y', strtotime($article['date'])) ?></time>
+        <h2><?= $article['title'] ?></h2>
+        <a href="<?= ROOT ?>/view/<?= $article['id'] ?>">Read more</a>
+        <?php if (is_auth()): ?>
+            <a href="<?= ROOT ?>/edit/<?= $article['id'] ?>">Edit</a>
+            <a href="<?= ROOT ?>/delete/<?= $article['id'] ?>"
+               onclick="return confirm('Are you sure you want to delete this article?')">Delete</a>
+        <?php endif; ?>
+    </div>
 <?php endforeach; ?>
-
